2e1b44d3114d3f9b024534661427b2d2fa3095a1
evaalvarezz <evaalvarez041@gmail.com>
2023-05-04 13:20:28 +0200

    dale

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import render_template,request,redirect,url_for
+from flaskext.mysql import MySQL
+from flask import send_from_directory
+from datetime import datetime
+import os
+
+app=Flask(__name__)
+
+mysql=MySQL()
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='comidas'
+mysql.init_app(app)
+
+
+
+
+@app.route('/')
+def index():
+    sql="SELECT *FROM `recetas`;"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    empleados=cursor.fetchall()
+    print(empleados)
+    
+    conn.commit()
+
+    return render_template('/src/index.html')
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
