b4b0fce6ac6b2a618bdf9d3be1b32a914afc4c48
evaalvarezz <evaalvarez041@gmail.com>
2023-05-22 16:27:11 +0200

    bdfree

--- api3/app.py
+++ api3/app.py
@@ -12,12 +12,11 @@ app.secret_key="develoteca"
 
 
 mysql=MySQL()
-app.config['MYSQL_DATABASE_HOST']='localhost'
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_HOST']='db4free.net'
+app.config['MYSQL_DATABASE_USER']='evaalvarez112112'
+app.config['MYSQL_DATABASE_PASSWORD']='Holamundo2002@'
 app.config['MYSQL_DATABASE_DB']='comidas'
 mysql.init_app(app)
-
 
 CARPETA=os.path.join('imagenes')
 app.config['CARPETA']=CARPETA
